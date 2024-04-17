90c6f3bd5b90ed780b5d61e3edf444c901d386ca
Michael Bleier <michael.bleier@uni-wuerzburg.de>
2024-04-17 16:16:15 +0200

    Updated rtc tool.

--- tools/setTime.py
+++ tools/setTime.py
@@ -1,7 +1,14 @@
 import serial 
 import time 
+import json
+import os
 
-sensorcube = serial.Serial(port='/dev/ttyACM0', baudrate=921600)
+# load config file (same directory as this file)
+configFile = open(os.path.dirname(os.path.abspath(__file__)) + '/config.json')
+config = json.load(configFile)
+
+# open serial port
+sensorcube = serial.Serial(port=config["serial_port"], baudrate=config["serial_baudrate"])
 
 # wait for full second
 while int(time.time() * 1000) % 1000 != 0:
@@ -9,7 +16,7 @@ while int(time.time() * 1000) % 1000 != 0:
 
 t = time.time()
 command = '{"time":' + str(int(t)) + ',"messages":["heartbeat"]}\r\n';
-print("Set time: " + str(t))
+print("Set time " + str(t) + ".")
 
 sensorcube.write(bytes(command, 'utf-8'))
 

0f69c6abf174c7167dfd424907661dba018939fa
Michael Bleier <michael.bleier@uni-wuerzburg.de>
2024-04-18 00:23:07 +0200

    Added option to overwrite log.

--- tools/recordMessages.py
+++ tools/recordMessages.py
@@ -7,12 +7,16 @@ import time
 parser = argparse.ArgumentParser(description='Print and record messages.')
 parser.add_argument('-m', '--message', dest='message', action='append', type=str, help='Message type, e.g., "heartbeat".', required=True)
 parser.add_argument('-f', '--file', dest='file', type=str, help='Output filename for recording messages.')
+parser.add_argument('--overwrite', help='Overwrite exisiting files.', action="store_true")
 args = parser.parse_args()
 
 file = None
 if args.file is not None:
     print('Recording to file "' + args.file +'".')
-    file = open(args.file, 'x')
+    if args.overwrite:
+        file = open(args.file, 'w')
+    else:
+        file = open(args.file, 'x')
 
 messageString = ""
 for m in args.message:
